328796b7653437e486b77e422370adbed13a63fc
ASalami99 <abdullah.salami@pau.edu.ng>
2022-10-22 13:14:27 +0100

    (My second commit)

--- Projects/Project_1.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-	let p = 520_000_000;
-	let r = 10;
-	let n = 5;
-	
-	let amount = p * ((1+(r/100))^n);
-	let compound_interest = amount - p;  
-	println!("The Amount is {} so therefore, the Compound Interest is {}",amount,compound_interest);
-}
--- Projects/Project_3.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-	let p = 210_000;
-	let r = 5;
-	let n = 3;
-	
-	let amount = p * (1 - (r / 100)) ^ n;
-	let compound_interest = amount - p;  
-	println!("The value of the TV set after 3 years is {} ",amount);
-}
